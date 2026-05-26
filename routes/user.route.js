b1a522ce2c93b8e730f1a86006ac4a6cb7666acc
deveBrice <brice.william971@gmail.com>
2026-05-26 11:43:07 +0200

    authentication

---
+++ routes/user.route.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+const userCtrl = require('../controllers/user.controller');
+
+router.post('/signup', userCtrl.signup);
+router.post('/signin', userCtrl.signin);
+router.get('/user', userCtrl.user);
+router.get('/logout', userCtrl.logout);
+
+module.exports = router;
